2153e2e5b30770fe7284a1e80b73bbf4f9767cff
Mohamedhassan2002 <Mohamed.hassan.2002.12345@gmail.com>
2021-04-11 21:05:04 +0200

    new repo

---
+++ js/custom.js
@@ -0,0 +1,160 @@
+// start button go-to-top
+var myButton =document.getElementById("scroll");
+
+window.onscroll = function(){
+
+    if (window.pageYOffset >= 200){
+
+        myButton.style.display = "block";
+    }
+
+    else {
+
+        myButton.style.display = "none";
+
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(function(){
+    $(".contant-1").click(function(){
+        $(".contant-4").slideToggle("slow")
+      });  
+      // 
+      $(".contant-2").click(function(){
+        $(".contant-5").slideToggle("slow")
+      });
+      //
+      $(".contant-3").click(function(){
+        $(".contant-6").slideToggle("slow")
+//////////////////////////////////////////////////
+       $(".contant-1").click(function(){
+           $(".contant-5 , .contant-6").slideUp("slow")});
+             //////
+             $(".contant-2").click(function(){
+                $(".contant-4 , .contant-6").slideUp("slow")});
+             ///////
+             $(".contant-3").click(function(){
+                $(".contant-4 , .contant-5").slideUp("slow")});
+    });      
+});*/
+////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+/*$(function(){
+    var newTask = $(".insert-task");
+    $(".add-task").on("submit", function(e){
+        e.preventDefault();
+        if(newTask.val() != ""){
+            $("<li class='list-group-item'>"+ newTask.val() +"</li>").appendTo("ul");
+            newTask.val("");
+        }
+    });
+    $("ul").on("click","li",function(){
+        $(this).css("text-decoration","line-through").delay(200).fadeOut(400, function(){
+        })
+        $(this).remove();
+    });
+});*/
+
+/////////////////////////////////////////////////////////////
+
+/*$(function(){
+      $(".blurelement").blur(function(){
+         checkerror(); 
+      });
+      $(".submit").click(function(){
+          checkerror();
+      })
+
+
+function checkerror()
+{
+    var emptyMail = $(".email").val() ;
+          if (emptyMail == ""){
+              $(".email").addClass("error");
+              $("#emailHelp").slideDown(500);
+              
+          }else if(emptyMail.length <= 4){
+            $(".email").addClass("error");
+            $("#emailHelp").slideDown(500);
+          }else{
+              $(".email").addClass("success");
+              $("#emailHelp").slideUp(500);
+          }
+          var emptyPassword = $(".Password").val() ;
+        if (emptyPassword == ""){
+            $(".Password").addClass("error");
+            $("#passwordHelp").slideDown(500);
+
+        }else if(emptyPassword.length <= 4){
+          $(".Password").addClass("error");
+          $("#passwordHelp").slideDown(500);
+
+        }else{
+            $(".Password").addClass("success");
+            $("#passwordHelp").slideUp(500);
+
+        }
+}
+ 
+    });*/
